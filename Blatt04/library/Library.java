7ebab4970898d29c069b2397d07489f7f6fa4cd1
Per Starke <perstarke@Pers-MBP.multi.box>
2021-05-10 19:07:49 +0200

    Added Library for task 3

---
+++ Blatt04/library/Library.java
@@ -0,0 +1,18 @@
+package library;
+import util.List;
+
+public class Library {
+
+    public void addItem(LibraryItem item){
+
+    }
+
+    public void deleteItem(LibraryItem item){
+
+    }
+
+    public List search(String text){
+        List list = new List()
+    }
+
+}
