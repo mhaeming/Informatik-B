5be3a5b184dfa3ccd59bb34c81d6124500cd01b2
Maxoz99 <maximilian@haeming.de>
2021-05-27 10:37:14 +0200

    feat: equals method for GenericEntry

--- Blatt06/genericList/GenericEntry.java
+++ Blatt06/genericList/GenericEntry.java
@@ -23,4 +23,36 @@ class GenericEntry<T> {
       this.o = o;
       this.next = e;
    }
+
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj)
+         return true;
+      if (obj == null)
+         return false;
+      if (getClass() != obj.getClass())
+         return false;
+      @SuppressWarnings("unchecked") GenericEntry<T> other = (GenericEntry<T>) obj;
+      if (o == null) {
+         if (other.o != null)
+            return false;
+      } else if (!o.equals(other.o))
+         return false;
+      if (next == null) {
+         if (other.next != null)
+            return false;
+      } else if (!next.equals(other.next))
+         return false;
+
+      return true;
+   }
+
+   @Override
+   public int hashCode() {
+      int prime = 13;
+      int result = 1;
+      result *= prime + o.hashCode();
+      result *= prime + next.hashCode();
+      return result;
+   }
 }
