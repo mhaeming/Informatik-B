da42f1377cfd665253dd97da490361663f4eee40
Per Starke <perstarke@Pers-MBP.multi.box>
2021-06-04 18:22:14 +0200

    Added comment for termination condition

--- Blatt07/visitor/PrintListVisitor.java
+++ Blatt07/visitor/PrintListVisitor.java
@@ -5,9 +5,12 @@ public class PrintListVisitor<E> implements Visitor<E>{
     @Override
     public boolean visit(Object o) {
         System.out.println(o);
+
+        // Optional: termination condition (without this, all elements are visited
         if(o.equals(42)){
             return false;
         }
+
         return true;
     }
 }
