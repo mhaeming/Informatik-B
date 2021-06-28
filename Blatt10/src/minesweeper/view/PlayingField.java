46d97d4640ad75f5ae28dfd8a9a02302e268ec3b
Per Starke <perstarke@Pers-MBP.fritz.box>
2021-06-28 22:36:03 +0200

    Initial commit: Blatt10

---
+++ Blatt10/src/minesweeper/view/PlayingField.java
@@ -0,0 +1,15 @@
+package minesweeper.view;
+
+import javax.swing.*;
+import java.util.Observable;
+import java.util.Observer;
+
+public class PlayingField extends JPanel implements Observer {
+
+    
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
