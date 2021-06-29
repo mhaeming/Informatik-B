78e72de7f412df296bd29a78e9f8eeea3ac2816a
Maximilian Häming <Maximilian@haeming.de>
2021-06-29 09:29:39 +0200

    Revert "Cleanup between two initial commits"
    This reverts commit 6061df4e8ac67d16a3ab43630eb1df34e3a48e16.

---
+++ Blatt10/Main.java
@@ -0,0 +1,11 @@
+import minesweeper.model.Board;
+
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board(5, 4, 3);
+        board.print();
+    }
+}
