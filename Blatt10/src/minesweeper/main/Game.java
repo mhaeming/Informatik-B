6f9663a776d0d0c8808907ee1bccf6de67afc658
Per Starke <perstarke@Pers-MBP.fritz.box>
2021-06-29 17:25:43 +0200

    Command line input

--- Blatt10/src/minesweeper/main/Game.java
+++ Blatt10/src/minesweeper/main/Game.java
@@ -10,10 +10,25 @@ import javax.swing.*;
  */
 public class Game {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println(args.length);
+            throw new IllegalArgumentException("Args length must be 3: Width, height, bombs");
+        }
+
+        int arguments[] = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            try {
+                arguments[i] = Integer.parseInt(args[i]);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Args must be Numeric!");
+            }
+        }
+
         JFrame frame = new JFrame("MineSweeper");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Board model = new Board(6, 4, 1);
+        Board model = new Board(arguments[0], arguments[1], arguments[2]);
         PlayingField view = new PlayingField(model);
         frame.setContentPane(view);
         frame.pack();
